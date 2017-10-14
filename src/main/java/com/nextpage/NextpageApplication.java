57b2f09761dc166cd89eebe417172513319e8dc3
Cancerpio <breezesky627@gmail.com>
2017-10-14 19:42:44 +0800

    initial mvn web-jersey-thymeleaf project created by springSTS

---
+++ src/main/java/com/nextpage/NextpageApplication.java
@@ -0,0 +1,12 @@
+package com.nextpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextpageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextpageApplication.class, args);
+	}
+}
